b3a9292a9b1ac0f25c7a5e8c854dd7d846d5b78f
Sparsh <sparsh0987654321@gmail.com>
2022-03-01 22:31:14 +0530

    change backend url from local host to given one

--- vevestaX/vevesta.py
+++ vevestaX/vevesta.py
@@ -292,7 +292,7 @@ class Experiment(object):
 
         # api-endpoint
         token = open("access_token.txt", "r").read()
-        backend_url = 'http://localhost:8082/VevestaX'
+        backend_url = 'https://api.matrixkanban.com/services-1.0-SNAPSHOT/VevestaX'
         headers = {
             'Authorization': 'Bearer ' + token,
             'Access-Control-Allow-Origin': '*',
