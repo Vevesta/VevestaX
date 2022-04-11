08b5ba5db0474c17b212e65b2b20cd4d5e9b623c
Priyanka-Vevesta <priyanka@vevesta.com>
2022-04-11 09:58:45 +0530

    added missing library requests to setup.py and made requirement.txt

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Stupidly simple library to track machine learning experiments as well as features',
     author='Vevesta Labs',
     license='Apache 2.0',
-    install_requires=['pandas','ipynbname','datetime','openpyxl','xlrd'],
+    install_requires=['pandas','ipynbname','datetime','openpyxl','xlrd','requests'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
