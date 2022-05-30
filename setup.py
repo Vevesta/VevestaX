6d8427b5cb9a660da4e07d9bc93d996876a82bb1
Aman sharma <sharma9917375201@gmail.com>
2022-05-30 17:05:52 +0530

    changing vevesta version from 5.4.0 to 5.5.0

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
 
     name='vevestaX',
     packages=find_packages(include=['vevestaX']),
-    version='5.3.0',
+    version='5.5.0',
     description='Stupidly simple library to track machine learning experiments as well as features',
     author='Vevesta Labs',
     license='Apache 2.0',
-    install_requires=['pandas','Jinja2','ipynbname','datetime','openpyxl','xlrd','requests','matplotlib','pyspark','numpy','scipy','statistics'],
+    install_requires=['pandas','Jinja2','ipynbname','datetime','openpyxl','xlrd','requests','matplotlib','pyspark','numpy','scipy','statistics','itertools'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
