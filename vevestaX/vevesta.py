328be73f6aecefcc75bf00b5f440fb8e37a3ed77
Jahnavi Sana <43995967+jahnavisana@users.noreply.github.com>
2022-07-06 17:56:58 +0530

    Update vevesta.py

--- vevestaX/vevesta.py
+++ vevestaX/vevesta.py
@@ -792,8 +792,8 @@ class Experiment(object):
         plt.close()
         pdffile="EDA.pdf"
         images=[ValueImageFile,ValueRatioImageFile,NumericalFeatureDistributionImageFile,NonNumericFeaturesImgFile,FeatureHistogramImageFile,OutliersImageFile]
-        a4inpt = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
-        layout_fun = img2pdf.get_layout_fun(a4inpt)
+        a4sheetinputsize = (img2pdf.mm_to_pt(210),img2pdf.mm_to_pt(297))
+        layout_function = img2pdf.get_layout_fun(a4inpt)
         file=[]
         for i in images:
             if os.path.exists(os.path.join(directoryToDumpData,i)):
